19761a121b9d96614b6df4ab90fda609129121bf
David Dang <dtudang@gmail.com>
2015-01-26 11:42:09 -0800

    completed

---
+++ app/controllers/pairings_controller.rb
@@ -0,0 +1,5 @@
+class PairingsController < ApplicationController
+
+
+
+end
---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,25 @@
+class WelcomeController < ApplicationController
+
+def index
+
+  @students = ["Michael Smykowski", "Edozie Izegbu",
+    "Nina Bachvarova", "Irina Tagintseva",
+    "Max Gordon",  "Dana Mulder",
+    "Tina Hweicheng Loh",  "Zubair Desai",
+    "Helen Lam", "Ngan Pham",
+    "Julien Mayer",  "Bradley Johnson",
+    "Luis Manzo",  "Cameron Irmas",
+    "Erika Lim", "David Dang",
+    "Michael Dean",  "Rosa Choi",
+    "Rory Grant",  "Oscar Linares",
+    "Kevin Litchfield",  "Denise Shephard",
+    "Heather Farley",  "Brady Stilwell",
+    "Victoria Billingsley",  "Kristian Ziel",
+    "Lauren Richie", "Alex Sell"]
+end
+
+
+
+
+
+end
